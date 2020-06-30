11a91c978c6eb6d2d8c7391dd3394b5e91b7dedc
edz <1191130744@qq.com>
2020-06-30 08:42:29 +0800

    homework

--- javaSE/club/banyuan/June29/HomeWork/Main.java
+++ javaSE/club/banyuan/June29/HomeWork/Main.java
@@ -5,6 +5,13 @@ public class Main {
         WeekDay sat=WeekDay.SATURDAY;
         WeekDay[] days= WeekDay.values();
         for (WeekDay day : days) {
+            if(day.isHoliday(day.name())){
+                System.out.println(day.toString()+"是周末");
+            }else {
+                System.out.println(day.toString()+"是工作日");
+            }
+        }
+        for (WeekDay day : days) {
             System.out.println(day.compareTo(sat));
         }
     }
